85dbb38c1f2f94a39903ae85421cc200f379931e
Shahzod010299 <shakhzodtoshboyev010299@gmail.com>
2023-05-14 19:57:14 +0500

    first comment

---
+++ android/app/src/main/kotlin/com/example/mening_dasturim/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mening_dasturim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Xursanova Nilufars'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network('https://upload.wikimedia.org/wikipedia/commons/7/7e/Dtpi.png',height: 300,),
+            Image.network('https://static.abt.uz/crop/720_420_603a0aba942e6.jpg',height: 300,),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
